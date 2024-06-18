e6c742e58c4be24496fb645de2e634d44672c540
neeratyoy <neeratyoy@gmail.com>
2024-06-18 13:21:17 +0200

    Updating basics

--- config.sh
+++ config.sh
@@ -11,6 +11,14 @@ ABSOLUTE_PATH="$(realpath "${RELATIVE_PATH}")"
 ### User Configuration: uses arguments $1 and $2 if passed or defaults
 export ENV_NAME="${1:-$(basename "$ABSOLUTE_PATH")}"  # Default Name of the venv is the directory that contains this file
 ENV_DIR="${2:-${ABSOLUTE_PATH}/venv}"  # Default location of this VENV is "./venv"
+export VENV_DIR=$(realpath "${ENV_DIR}")
 export ENV_DIR=${ENV_DIR}${ENV_NAME}
+export VENV_REPO_DIR="${ABSOLUTE_PATH}"
+
+echo ""
+echo "\$ENV_NAME: "$ENV_NAME
+echo "\$ENV_DIR: "$ENV_DIR
+echo "\$VENV_DIR: "$VENV_DIR
+echo "\$VENV_REPO_DIR: "$VENV_REPO_DIR
 
 # end of file
